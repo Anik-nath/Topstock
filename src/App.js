d48d93ded49a66961d04910f28d1ae0af9222927
Anik-Nath <anikdevnath03112000@gmail.com>
2022-01-12 23:21:13 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./Components/Pages/Home/Home";
 import About from "./Components/Pages/About/About";
+import Navigations from "./Components/Shared/Navigations/Navigations";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navigations></Navigations>
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/home" element={<Home></Home>}></Route>
--- src/Components/Shared/Navigations/Navigations.js
+++ src/Components/Shared/Navigations/Navigations.js
@@ -1,11 +1,83 @@
-import React from 'react';
-
+import React from "react";
+import {
+  Badge,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import { HashLink } from "react-router-hash-link";
 const Navigations = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <Navbar
+        sticky="top"
+        className="py-3"
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+      >
+        <Container>
+          <Navbar.Brand as={HashLink} to="/home" className="fs-2">
+            <span style={{ color: "orangered" }} className="fw-bold">
+              Top
+            </span>
+            Stock
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav style={{ fontWeight: "500" }} className="me-auto">
+              <Nav.Link className="mx-lg-2" as={HashLink} to="/home">
+                Home
+              </Nav.Link>
+              <Nav.Link className="mx-lg-2" as={HashLink} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link className="mx-lg-2" as={HashLink} to="/shop">
+                Shop
+              </Nav.Link>
+              <Nav.Link className="mx-lg-2" as={HashLink} to="/contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Form className="my-2 my-lg-0">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2 rounded-pill border-0"
+                  aria-label="Search"
+                />
+              </Form>
+            </Nav>
+            <Nav style={{ fontWeight: "500" }}>
+              <Nav.Link className="mx-lg-2" as={HashLink} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={HashLink} to="/cart">
+                <i className="fas fa-shopping-cart position-relative fs-5">
+                  <Badge
+                    bg="warning"
+                    className="position-absolute start-50 bottom-50 px-2 rounded-circle"
+                  >
+                    2
+                  </Badge>
+                </i>
+              </Nav.Link>
+              <Nav.Link as={HashLink} to="/wishlist">
+                <i className="far fa-heart mx-lg-2 fs-5"></i>
+              </Nav.Link>
+              <Nav.Link eventKey={2} href="#user">
+                <i className="fas fa-user-circle fs-5"></i>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Navigations;
