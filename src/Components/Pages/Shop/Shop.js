ef091a078b6b2b982b4b50229068e1116bc93095
Anik-Nath <anikdevnath03112000@gmail.com>
2022-01-15 21:05:06 +0600

    added some features and loading spinner

--- src/Components/Pages/Shop/Shop.js
+++ src/Components/Pages/Shop/Shop.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Accordion, Card, Col, Container, Row, useAccordionButton } from "react-bootstrap";
+import {
+  Accordion,
+  Card,
+  Col,
+  Container,
+  Row,
+  useAccordionButton,
+  Spinner,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { ChangeTitle } from "../../../titlechange/titlechange";
 
@@ -16,13 +24,13 @@ const Shop = () => {
 
   function CustomToggle({ children, eventKey }) {
     const decoratedOnClick = useAccordionButton(eventKey, () =>
-      console.log('totally custom!'),
+      console.log("totally custom!")
     );
-  
+
     return (
       <span
         type="button"
-        style={{ backgroundColor: 'none' }}
+        style={{ backgroundColor: "none" }}
         onClick={decoratedOnClick}
       >
         {children}
@@ -31,7 +39,6 @@ const Shop = () => {
   }
 
   return (
-    
     <div>
       <div
         id="banner"
@@ -65,12 +72,14 @@ const Shop = () => {
         <Container>
           <Row>
             <Col lg={3} md={3} sm={12} xs={12}>
-              <h5 className="fw-bold">Product Categories</h5>
-              <Accordion defaultActiveKey="0">
+              <h5 className="fw-bold mb-3">Product Categories</h5>
+              <Accordion>
                 <div>
                   <div className="d-flex justify-content-between bg-white">
                     <span className="fs-6">Accesories</span>
-                    <CustomToggle eventKey="0"><i className="fas fa-chevron-right"></i></CustomToggle>
+                    <CustomToggle eventKey="0">
+                      <i className="fas fa-chevron-right"></i>
+                    </CustomToggle>
                   </div>
                   <Accordion.Collapse eventKey="0">
                     <Card.Body>
@@ -79,42 +88,97 @@ const Shop = () => {
                     </Card.Body>
                   </Accordion.Collapse>
                 </div>
-                <div>
+                <div className="my-3">
                   <div className="d-flex justify-content-between bg-white">
-                    <span className="fs-6">Accesories</span>
-                    <CustomToggle eventKey="1"><i className="fas fa-chevron-right"></i></CustomToggle>
+                    <span className="fs-6">Clothing</span>
+                    <CustomToggle eventKey="1">
+                      <i className="fas fa-chevron-right"></i>
+                    </CustomToggle>
                   </div>
                   <Accordion.Collapse eventKey="1">
                     <Card.Body>
-                      <p className="mb-1">Floor</p>
-                      <p className="mb-1">Outdoor</p>
+                      <p className="mb-1">Men</p>
+                      <p className="mb-1">Women</p>
+                      <p className="mb-1">Children</p>
                     </Card.Body>
                   </Accordion.Collapse>
                 </div>
-
-                
+                <div>
+                  <div className="d-flex justify-content-between bg-white">
+                    <span className="fs-6">Interior</span>
+                    <CustomToggle eventKey="2">
+                      <i className="fas fa-chevron-right"></i>
+                    </CustomToggle>
+                  </div>
+                  <Accordion.Collapse eventKey="2">
+                    <Card.Body>
+                      <p className="mb-1">Men</p>
+                      <p className="mb-1">Women</p>
+                      <p className="mb-1">Children</p>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </div>
+                <div className="my-3">
+                  <div className="d-flex justify-content-between bg-white">
+                    <span className="fs-6">Lights</span>
+                    <CustomToggle eventKey="3">
+                      <i className="fas fa-chevron-right"></i>
+                    </CustomToggle>
+                  </div>
+                  <Accordion.Collapse eventKey="3">
+                    <Card.Body>
+                      <p className="mb-1">Bathroom lights</p>
+                      <p className="mb-1">Wall lights</p>
+                      <p className="mb-1">Table lamps</p>
+                      <p className="mb-1">Celling lights</p>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </div>
               </Accordion>
               <h5 className="fw-bold py-2">Filter By Price</h5>
-              <h5 className="fw-bold">Popular Product</h5>
-
+              <div>
+                <label for="customRange3" class="form-label">
+                  Price : $0 - $400
+                </label>
+                <input
+                  type="range"
+                  class="form-range"
+                  min="0"
+                  max="400"
+                  step="50"
+                  id="customRange3"
+                />
+              </div>
             </Col>
             <Col lg={9} md={9} sm={12} xs={12}>
               <Row className="g-3">
+                {products.length === 0 && (
+                  <div className="mx-auto text-center">
+                    {" "}
+                    <Spinner animation="grow" variant="warning" />
+                  </div>
+                )}
                 {products.map((product) => (
-               <Col key={product._id} lg={4} sm={12} xs={12} md={6}>
-                 <div className="card bg-light">
-                 <Link to={`/productdetials/${product._id}`}>
-                     <div style={{height:"300px"}}> 
-                         <img className="w-100 h-100 rounded" src={product.image} alt="" />
-                     </div>
-                  </Link>
-                 </div>
-                 <div className="bg-white d-flex flex-column pt-2">
-                   <span className="fs-6">{product.title}</span>
-                    <span className="fs-6 fw-bold text-dark">${product.price}</span>
-                   </div>
-               </Col>
-            ))}
+                  <Col key={product._id} lg={4} sm={12} xs={12} md={6}>
+                    <div className="card bg-light">
+                      <Link to={`/productdetials/${product._id}`}>
+                        <div style={{ height: "300px" }}>
+                          <img
+                            className="w-100 h-100 rounded"
+                            src={product.image}
+                            alt=""
+                          />
+                        </div>
+                      </Link>
+                    </div>
+                    <div className="bg-white d-flex flex-column pt-2">
+                      <span className="fs-6">{product.title}</span>
+                      <span className="fs-6 fw-bold text-dark">
+                        ${product.price}
+                      </span>
+                    </div>
+                  </Col>
+                ))}
               </Row>
             </Col>
           </Row>
